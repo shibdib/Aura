7ce2ab69b05d6b669dd059ecd4a8760a3949ef2d
shibdib <supernfadmin@gmail.com>
2018-06-02 22:25:12 -0400

    fix killmarks

--- aura/extensions/eve_rpg/eve_rpg.py
+++ aura/extensions/eve_rpg/eve_rpg.py
@@ -27,6 +27,7 @@ class EveRpg:
                 await self.process_anomaly_ratting()
                 await self.process_roams()
                 await self.process_ganks()
+                await self.process_users()
                 await asyncio.sleep(12)
             except Exception:
                 self.logger.exception('ERROR:')
