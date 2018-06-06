82c6d5d3e4ceedf9fe675915a6ebc67c20599724
shibdib <supernfadmin@gmail.com>
2018-06-06 08:59:47 -0400

    multisell

--- aura/extensions/assets/assets.py
+++ aura/extensions/assets/assets.py
@@ -43,25 +43,42 @@ class Assets:
             if player[0][15] is not None:
                 ship_hangar = ast.literal_eval(player[0][15])
                 stored_ships_array = []
+                count = 0
                 for key, ships in ship_hangar.items():
                     for ship in ships:
                         region_name = await game_functions.get_region(key)
                         ship_name = await game_functions.get_ship_name(int(ship['ship_type']))
                         stored_ships_array.append('{} - {}'.format(ship_name, region_name))
-                stored_ships = '\n'.join(stored_ships_array)
-                embed.add_field(name="Ships",
-                                value='{}'.format(stored_ships))
+                        if count >= 10:
+                            count = 0
+                            stored_ships = '\n'.join(stored_ships_array)
+                            embed.add_field(name="Ships",
+                                            value='{}'.format(stored_ships))
+                            stored_ships_array = []
+                if len(stored_ships_array) > 0:
+                    stored_ships = '\n'.join(stored_ships_array)
+                    embed.add_field(name="Ships",
+                                    value='{}'.format(stored_ships))
             if player[0][13] is not None:
                 module_hangar = ast.literal_eval(player[0][13])
                 stored_modules_array = []
+                count = 0
                 for key, items in module_hangar.items():
                     for item in items:
                         region_name = await game_functions.get_region(key)
                         module_name = await game_functions.get_module_name(int(item))
                         stored_modules_array.append('{} - {}'.format(module_name, region_name))
-                stored_modules = '\n'.join(stored_modules_array)
-                embed.add_field(name="Modules",
-                                value='{}'.format(stored_modules))
+                        count += 1
+                        if count >= 10:
+                            count = 0
+                            stored_modules = '\n'.join(stored_modules_array)
+                            embed.add_field(name="Modules",
+                                            value='{}'.format(stored_modules))
+                            stored_modules_array = []
+                if len(stored_modules_array) > 0:
+                    stored_modules = '\n'.join(stored_modules_array)
+                    embed.add_field(name="Modules",
+                                    value='{}'.format(stored_modules))
             if player[0][13] is None and player[0][15] is None:
                 embed.add_field(name="Asset List",
                                 value='No Assets Found')
