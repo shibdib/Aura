9c57a1e9d68095f1ed913e9257a3fa165a5f6a2a
shibdib <supernfadmin@gmail.com>
2018-06-06 16:00:28 -0400

    add the ability to remove blues

--- aura/extensions/contacts/contacts.py
+++ aura/extensions/contacts/contacts.py
@@ -46,7 +46,8 @@ class Contacts:
                              text="Aura - EVE Text RPG")
             embed.add_field(name="{}'s Contacts".format(player_name),
                             value="__**Blues**__\n{}\n\n**1.** Add user to the blue list\n"
-                                  "2. Return to the main menu".format(blue_text))
+                                  "**2.** Remove a user from the blue list\n"
+                                  "**3.** Return to the main menu".format(blue_text))
             await ctx.author.send(embed=embed)
 
             def check(m):
@@ -57,6 +58,8 @@ class Contacts:
             if content == '1':
                 await self.add_blue(ctx, player[0])
             elif content == '2':
+                await self.remove_blue(ctx, player[0])
+            elif content == '3':
                 await ctx.invoke(self.bot.get_command("me"), True)
             elif '!!' not in content:
                 await ctx.author.send('**ERROR** - Not a valid choice.')
@@ -89,7 +92,7 @@ class Contacts:
                          text="Aura - EVE Text RPG")
         embed.add_field(name="Add Contacts".format(player_name),
                         value="Confirm you want to add **{}** to your blue list.\n\n"
-                              "*Players on your blue list will not be attacked by you, they will still attack you"
+                              "*Players on your blue list will not be attacked by you, they will still attack you "
                               "unless they also add you to their blues list*\n\n"
                               "**1.** Yes\n"
                               "**2.** No".format(blue_name))
@@ -115,4 +118,69 @@ class Contacts:
         values = (str(blue_array), ctx.author.id,)
         await db.execute_sql(sql, values)
         await ctx.author.send('**Success** - {} is now blue.'.format(blue_name))
+        await ctx.invoke(self.bot.get_command("me"), True)
+
+    async def remove_blue(self, ctx, player):
+        if player[0][21] is not None:
+            remove_blue = {}
+            remove_text = []
+            count = 1
+            for user in ast.literal_eval(player[0][21]):
+                sql = ''' SELECT * FROM eve_rpg_players WHERE `id` = (?) '''
+                values = (user,)
+                blue = await db.select_var(sql, values)
+                blue_name = self.bot.get_user(int(blue[0][2])).display_name
+                remove_blue[count] = {'name': blue_name, 'id': user, 'selection': count}
+                remove_text.append('**{}.** {}'.format(count, blue_name))
+            blue_text = '\n'.join(remove_text)
+        else:
+            await ctx.author.send('**ERROR** - You have no one to remove.')
+            return await ctx.invoke(self.bot.get_command("me"), True)
+        player_name = self.bot.get_user(int(player[2])).display_name
+        embed = make_embed(icon=ctx.bot.user.avatar)
+        embed.set_footer(icon_url=ctx.bot.user.avatar_url,
+                         text="Aura - EVE Text RPG")
+        embed.add_field(name="Remove Contacts".format(player_name),
+                        value="Select the user you'd like to remove\n\n{}".format(blue_text))
+        await ctx.author.send(embed=embed)
+
+        def check(m):
+            return m.author == ctx.author and m.channel == ctx.author.dm_channel
+
+        msg = await self.bot.wait_for('message', check=check, timeout=120)
+        content = msg.content
+        if int(content) not in remove_blue:
+            await ctx.author.send('**ERROR** - Incorrect Selection.')
+            return await ctx.invoke(self.bot.get_command("me"), True)
+        sql = ''' SELECT * FROM eve_rpg_players WHERE `id` = (?) '''
+        values = (remove_blue[int(content)]['id'],)
+        blue = await db.select_var(sql, values)
+        blue_name = self.bot.get_user(int(blue[0][2])).display_name
+        embed = make_embed(icon=ctx.bot.user.avatar)
+        embed.set_footer(icon_url=ctx.bot.user.avatar_url,
+                         text="Aura - EVE Text RPG")
+        embed.add_field(name="Add Contacts".format(player_name),
+                        value="Confirm you want to remove **{}** from your blue list.\n\n"
+                              "*You will freely engage this player if you encounter them*\n\n"
+                              "**1.** Yes\n"
+                              "**2.** No".format(blue_name))
+        await ctx.author.send(embed=embed)
+
+        def check(m):
+            return m.author == ctx.author and m.channel == ctx.author.dm_channel
+
+        msg = await self.bot.wait_for('message', check=check, timeout=120)
+        confirm = msg.content
+        if int(confirm) != 1:
+            await ctx.author.send('**Removal Canceled**')
+            return await ctx.invoke(self.bot.get_command("me"), True)
+        blue_array = ast.literal_eval(player[21])
+        blue_array.remove(remove_blue[int(content)]['id'])
+        sql = ''' UPDATE eve_rpg_players
+                    SET blue_players = (?)
+                    WHERE
+                        player_id = (?); '''
+        values = (str(blue_array), ctx.author.id,)
+        await db.execute_sql(sql, values)
+        await ctx.author.send('**Success** - {} is no longer blue.'.format(blue_name))
         await ctx.invoke(self.bot.get_command("me"), True)
