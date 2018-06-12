16dca0b5c7746c92f6f3abf9c4132d7ab9a6092b
shibdib <supernfadmin@gmail.com>
2018-06-12 05:57:09 -0400

    fix some travel bugs

--- aura/extensions/eve_rpg/eve_rpg.py
+++ aura/extensions/eve_rpg/eve_rpg.py
@@ -109,7 +109,8 @@ class EveRpg:
                 for camper in campers:
                     # Corp check
                     if camper[23] is not None:
-                        corp_array = ast.literal_eval(camper[23])
+                        corp_info = await game_functions.get_user_corp(camper[23])
+                        corp_array = ast.literal_eval(corp_info[23])
                         if traveler[0] in corp_array:
                             continue
                     # Blue check
@@ -585,7 +586,8 @@ class EveRpg:
                 for target in potential_targets:
                     # Corp check
                     if roamer[23] is not None:
-                        corp_array = ast.literal_eval(roamer[23])
+                        corp_info = await game_functions.get_user_corp(roamer[23])
+                        corp_array = ast.literal_eval(corp_info[23])
                         if target[0] in corp_array:
                             continue
                     # Blue check
